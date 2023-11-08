95908fd4565a1e3644fd49ddd3e1d13370f7eb82
lateef-taiwo <taiwolateef55@gmail.com>
2023-11-08 15:24:49 +0100

    added new terraform files for k8s cluster provisioning

---
+++ automate-k8s-cluster-deployment/vpc.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+data "aws_availability_zones" "available" {}
+
+
+module "myapp-vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+    version = "2.64.0"
+
+    name = "myapp-vpc"
+    cidr = var.vpc_cidr_block
+    private_subnets = var.private_subnet_cidr_blocks
+    public_subnets = var.public_subnet_cidr_blocks
+    azs = data.aws_availability_zones.available.names 
+    
+    enable_nat_gateway = true
+    single_nat_gateway = true
+    enable_dns_hostnames = true
+
+    tags = {
+        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    }
+
+    public_subnet_tags = {
+        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+        "kubernetes.io/role/elb" = 1 
+    }
+
+    private_subnet_tags = {
+        "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+        "kubernetes.io/role/internal-elb" = 1 
+    }
+
+}
